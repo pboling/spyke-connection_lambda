53dee360b49a8d8cf70535220c1793b312fa538d
Peter Boling <peter.boling@gmail.com>
2024-03-23 14:03:58 -0600

    🎉 First Commit Skeleton

---
+++ lib/spyke/connection_lambda.rb
@@ -0,0 +1,10 @@
+# frozen_string_literal: true
+
+require_relative "connection_lambda/version"
+
+module Spyke
+  module ConnectionLambda
+    class Error < StandardError; end
+    # Your code goes here...
+  end
+end
